feb7b76254515a1b3b9a0cf0052ed175dd04a731
Esmé Taylor Richardson <esme.taylor.richardson@gmail.com>
2024-09-01 00:09:28 -0400

    restyled to be flat

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -22,23 +22,26 @@ function Search() {
   }
 
   return (
-    <div className="flex items-center justify-center backdrop-blur-3xl bg-white/30 border-2 border-zinc-900 pt-8 pb-8 pl-8 pr-8 rounded-xl drop-shadow-md">
+    <div className="flex items-center justify-center backdrop-blur-3xl bg-white/30 border-2 border-zinc-900 pt-8 pb-8 pl-8 pr-8 shadow-[8px_8px_0px_rgba(0,0,0,1)]">
       <form onSubmit={handleSubmit} className="flex flex-col items-center ">
       <div className='w-52 h-24 bg-saigalogo bg-contain bg-no-repeat mb-4'></div>
-        <div className="flex gap-4 items-center justify-center w-80 h-12 pr-8 pl-8 mt-2 mb-4 border-none rounded-md saturate-0 brightness-0">
+        <div className="flex gap-4 items-center justify-center w-80 h-12 pr-8 pl-8 mt-2 mb-4 border-none saturate-0 brightness-100 ">
           
-          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer ${activeEngine === 'Google' ? 'h-16 w-16' : ''}`} onClick={() => setActiveEngine('Google')} width="50" height="50" src="https://img.icons8.com/ios-filled/500/google-logo.png" alt="google-logo"/>
-          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer ${activeEngine === 'Wikipedia' ? 'h-16 w-16' : ''}`} onClick={() => setActiveEngine('Wikipedia')} width="50" height="50" src="https://img.icons8.com/ios-filled/500/wikipedia.png" alt="wikipedia"/>
-          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer ${activeEngine === 'Khan' ? 'h-16 w-16' : ''}`} onClick={() => setActiveEngine('Khan')} width="50" height="50" src="https://img.icons8.com/ios-filled/500/khan-academy.png" alt="khan-academy"/>
-          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer ${activeEngine === 'Youtube' ? 'h-16 w-16' : ''}`} onClick={() => setActiveEngine('Youtube')} width="50" height="50" src="https://img.icons8.com/ios-filled/500/youtube-play.png" alt="youtube-play"/>
+          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer opacity-100 ${activeEngine === 'Google' ? '' : ''}`} onClick={() => setActiveEngine('Google')} width="50" height="50" src={`${activeEngine === 'Google' ? 'https://img.icons8.com/ios-filled/500/google-logo.png' : 'https://img.icons8.com/ios/150/google-logo--v1.png'}`} alt="google-logo"/>
+          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer opacity-100 ${activeEngine === 'Wikipedia' ? '' : ''}`} onClick={() => setActiveEngine('Wikipedia')} width="50" height="50" src={`${activeEngine === 'Wikipedia' ? 'https://img.icons8.com/ios-filled/500/wikipedia.png' : 'https://img.icons8.com/ios/150/wikipedia.png'}`} alt="wikipedia"/>
+          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer opacity-100 ${activeEngine === 'Khan' ? '' : ''}`} onClick={() => setActiveEngine('Khan')} width="50" height="50" src={`${activeEngine === 'Khan' ? 'https://img.icons8.com/ios-filled/500/khan-academy.png' : 'https://img.icons8.com/ios/150/khan-academy.png'}`} alt="khan-academy"/>
+          <img className={`hover:scale-90 ease-in-out duration-100 cursor-pointer opacity-100 ${activeEngine === 'Youtube' ? '' : ''}`} onClick={() => setActiveEngine('Youtube')} width="50" height="50" src={`${activeEngine === 'Youtube' ? 'https://img.icons8.com/ios-filled/500/youtube-play.png' : 'https://img.icons8.com/ios/150/youtube-play--v1.png'}`} alt="youtube-play"/>
 
         </div>
-        <input
-          className="px-16 py-2 border-2 border-zinc-900 rounded-md outline-none mb-4 text-center drop-shadow"
-          type="text"
-          placeholder="What would you like to learn?"
-          name="query"
-        />
+        <div>
+          <input
+            className="px-16 py-2 border-2 border-zinc-900 outline-none mb-4 text-center drop-shadow"
+            type="text"
+            placeholder="What would you like to learn?"
+            name="query"
+          />
+          <button className={`border-t-2 border-b-2 border-r-2 border-zinc-900 px-4 py-2 cursor-default ${activeEngine !== '' ? 'hover:bg-zinc-900 hover:text-white hover:cursor-pointer ease-in-out duration-100' : ''}`}>Search</button>
+        </div>
         <em>“Study hard what interests you the most in the most undisciplined, irreverent and original manner possible.”</em>
         <strong>Richard Feynman</strong>
       </form>
