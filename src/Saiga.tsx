7246e5643e8e88c30718b14c0830cc890c28b08d
Esmé Taylor Richardson <esme.taylor.richardson@gmail.com>
2024-08-31 21:48:38 -0400

    restyled to have particles and engine options with search

--- src/Saiga.tsx
+++ src/Saiga.tsx
@@ -1,14 +1,18 @@
-import Panel from './components/Panel/Panel'
+import Stars from './components/Particles/Stars'
 import Search from './components/Search/Search'
 import './Saiga.css'
+
 
 function Saiga() {
 
   return (
-    <div className="flex flex-col max-w-svw mx-auto p-4 overflow-y-hidden h-screen items-center justify-center">
-      <Search />
-      <Panel />
-    </div>
+    <>
+      <Stars />
+      <div className="flex flex-col max-w-svw mx-auto p-4 overflow-y-hidden h-screen items-center justify-center fade-in select-none">
+        <Search />
+      </div>
+    </>
+
   )
 }
 
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import '../../index.css'
 function Search() {
+  const [activeEngine, setActiveEngine] = useState('Google');
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
   }
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center backdrop-blur-md bg-white/30 border-2 border-zinc-900 pt-8 pb-8 pl-8 pr-8 rounded-xl">
       <form onSubmit={handleSubmit} className="flex flex-col items-center ">
       <div className='w-52 h-24 bg-saigalogo bg-contain bg-no-repeat mb-4'></div>
+        <div className="flex gap-4 items-center justify-center w-80 h-12 pr-8 pl-8 mt-2 mb-4 border-none rounded-md saturate-0 brightness-0">
+          
+          <img className="hover:scale-90 ease-in-out duration-100 cursor-pointer" onClick={() => setActiveEngine('Google')} width="50" height="50" src="https://img.icons8.com/ios-filled/50/google-logo.png" alt="google-logo"/>
+          <img className="hover:scale-90 ease-in-out duration-100 cursor-pointer" onClick={() => setActiveEngine('Wikipedia')} width="50" height="50" src="https://img.icons8.com/ios-filled/50/wikipedia.png" alt="wikipedia"/>
+          <img className="hover:scale-90 ease-in-out duration-100 cursor-pointer" onClick={() => setActiveEngine('Khan')} width="50" height="50" src="https://img.icons8.com/ios-filled/50/khan-academy.png" alt="khan-academy"/>
+          <img className="hover:scale-90 ease-in-out duration-100 cursor-pointer" onClick={() => setActiveEngine('Youtube')} width="50" height="50" src="https://img.icons8.com/ios-filled/50/youtube-squared.png" alt="youtube-squared"/>
+
+        </div>
         <input
           className="px-16 py-2 border-2 border-zinc-900 rounded-md outline-none mb-4 text-center drop-shadow"
           type="text"
