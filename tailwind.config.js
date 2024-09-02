1446216647ab539e30650c4693a0a31591ad9552
Esmé Taylor Richardson <esme.taylor.richardson@gmail.com>
2024-09-02 16:52:33 -0400

    removed unused assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ export default {
       'sandal': {	50: '#FBFAF8',	100: '#E8DED5',	200: '#D4C3B2',	300: '#C1A78F',	400: '#A88A6D',	500: '#93785F',	600: '#7E6752',	700: '#685644',	800: '#534436',	900: '#3E3328',	950: '#29211A',},
       },
     backgroundImage: {
-      'saigamain': "url('./src/images/saigasnow.png')",
       'saigalogo': "url('./src/images/saigalogo.png')",
       },
   },
