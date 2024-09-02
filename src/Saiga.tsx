7223e7f02926142d2a350534fd60df2264878dfa
Esmé Taylor Richardson <esme.taylor.richardson@gmail.com>
2024-09-02 17:06:30 -0400

    changed icon and title

--- src/Saiga.tsx
+++ src/Saiga.tsx
@@ -4,7 +4,7 @@ import './Saiga.css'
 
 
 function Saiga() {
-  
+
   return (
     <>
       <div className="flex flex-col max-w-svw mx-auto p-4 overflow-y-hidden h-screen items-center justify-center fade-in select-none">
